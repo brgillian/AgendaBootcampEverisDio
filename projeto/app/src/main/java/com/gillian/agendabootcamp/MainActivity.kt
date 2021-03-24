7e4e2870d32ef15c6f6cf8c557458931d4023275
brgillian <brgillian@gmail.com>
2021-03-24 10:49:23 -0300

    criacao do intent

--- projeto/app/src/main/java/com/gillian/agendabootcamp/MainActivity.kt
+++ projeto/app/src/main/java/com/gillian/agendabootcamp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.gillian.agendabootcamp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract.CONTENT_URI
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSetEvent = findViewById(R.id.set_event) as Button
+        btnSetEvent.setOnClickListener{
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                
+        }
     }
 }
